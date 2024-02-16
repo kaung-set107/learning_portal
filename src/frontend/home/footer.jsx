e42cfb2f8dded526b0d414092b14d12ff3ceb390
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2024-02-16 09:53:00 +0630

    fix

--- src/frontend/home/footer.jsx
+++ src/frontend/home/footer.jsx
@@ -17,19 +17,24 @@ const Footer = () => {
             >
               Contact Us
             </h1>
-            <p className='pb-3'>
-              <FontAwesomeIcon icon={faLocationDot} size='lg' /> - Room
-              303,Third Floor Khattar Street Grand Residence Khattar Sanchaung
-              Township
-            </p>
-            <p className='pb-3'>
-              <FontAwesomeIcon icon={faPhone} size='lg' /> - +959-5103915,
-              +959422557884
-            </p>
-            <p className='pb-3'>
-              <FontAwesomeIcon icon={faEnvelope} size='lg' /> -
-              msiacademy2018@gmail.com
-            </p>
+            <div className='pb-3 flex gap-4'>
+              <FontAwesomeIcon icon={faLocationDot} size='lg' />{" "}
+              <p>
+                Room 303,Third Floor, Khattar Street, Grand Khattar Residence,
+                Sanchaung Township, Yanogn, Myanmar.
+              </p>
+            </div>
+            <div className='pb-3 flex gap-3'>
+              <FontAwesomeIcon icon={faPhone} size='lg' />{" "}
+              <p>+959 422 557 884</p>
+            </div>
+            <div className='pb-3 flex gap-4'>
+              <FontAwesomeIcon icon={faEnvelope} size='lg' />
+              <p className='flex flex-col gap-1'>
+                <span>info@msiacademy.edu.mm</span>
+                <span>academicdept@msiacademy.edu.mm</span>
+              </p>
+            </div>
             {/* <h1 className='font-[regular] text-2xl my-5'>About MSI Academy</h1>
                         <p className='font-[light]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesqe sit amet sapien.</p> */}
             {/* <img src="/assets/images/logo.jpeg" onClick={()=>router.push("/")} className='cursor-pointer w-[300px] p-5' alt="" /> */}
@@ -67,7 +72,7 @@ const Footer = () => {
         <div className='text-md flex items-center flex-wrap justify-between p-5 border-t-[0.5px] border-white'>
           <h1>Powered by NEOMOCA.</h1>
           <h1 className='my-5'>
-            © 2022 Myanmar Search International Academy. All right reserved.
+            © 2022 MSI Academy Education Centre. All right reserved.
           </h1>
         </div>
       </div>
