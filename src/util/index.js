415b78ad034b6b5e9bff084b4d7129990c99ac06
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2024-04-10 16:58:50 +0630

    fix

--- src/util/index.js
+++ src/util/index.js
@@ -1,15 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 export const getFile = ({ resource, fileType, payload }) => {
   let prefix = payload.destination.replace("public/assets/", "");
-  // return (
-  //   `http://learningportalbackend.kwintechnologies.com:3600/${prefix}/` +
-  //   payload.filename
-  // );
-
   return (
-    `http://localhost:3600/${prefix}/` +
+    `http://learningportalbackend.kwintechnologies.com:3600/${prefix}/` +
     payload.filename
-  )
+  );
+
+  // return (
+  //   `http://localhost:3600/${prefix}/` +
+  //   payload.filename
+  // )
 };
 
 export const download = (data) => {
